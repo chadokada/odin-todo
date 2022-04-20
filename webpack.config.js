9de24c54c0ec92eaa31d24365afd3c97a70aa6fb
chadokada <chadokada@gmail.com>
2022-04-19 21:34:06 -1000

    .

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        todo: './src/todo.js',
+        display: './src/display.js'
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        filename: '[name].main.js',
+        path: path.resolve(__dirname, 'dist'),
+        //clean: true,
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {            
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
